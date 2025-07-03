5bb653ba371b923a62ca34f0c2ee2c58bff51863
Ivan Yarigo <yarigo2018@yandex.ru>
2025-07-03 19:58:36 +0300

    Add Book model

---
+++ src/main/java/com/example/library/model/Book.java
@@ -0,0 +1,26 @@
+package com.example.library.model;
+
+public class Book {
+    Long id;
+    String title;
+    String author;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public String getAuthor() {
+        return author;
+    }
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
