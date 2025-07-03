f9a6faecc01a43d29b2f683ebb1a439c692ba98d
Ivan Yarigo <yarigo2018@yandex.ru>
2025-07-03 19:53:36 +0300

    Initial Spring Boot setup with hello endpoint

---
+++ src/main/java/com/example/library/controller/HelloController.java
@@ -0,0 +1,12 @@
+package com.example.library.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String Hello() {
+        return "Привет, Spring Boot!";
+    }
+}
