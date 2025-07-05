0205c7ea84e291c17a9b6a82872b71cf7120deee
Ivan Yarigo <yarigo2018@yandex.ru>
2025-07-05 19:51:48 +0300

    refactor: adapt BookService methods for BookRepository, remove ArrayList<Book> because we don't need it now, remove getBookById method because it was used for filter ArrayList<Book> books

--- src/main/java/com/example/library/service/BookService.java
+++ src/main/java/com/example/library/service/BookService.java
@@ -3,72 +3,70 @@ package com.example.library.service;
 import com.example.library.model.Book;
 import com.example.library.model.BookDTO;
 import com.example.library.model.BookPatchDTO;
+import com.example.library.repository.BookRepository;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class BookService {
-    private final List<Book> books = new ArrayList<>();
+    private final BookRepository bookRepository;
+
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     public List<BookDTO> getAllBooks() {
 
-        return books.stream()
+        return bookRepository.findAll().stream()
                 .map(BookMapper::toBookDTO)
                 .toList();
     }
 
     public BookDTO getDtoById(Long id) {
-        return books.stream()
+        return bookRepository.findById(id).stream()
                 .filter(book -> book.getId().equals(id))
                 .findFirst()
                 .map(BookMapper::toBookDTO)
                 .orElse(null);
     }
 
-    public Book getBookById(Long id) {
-        return books.stream()
-                .filter(book -> book.getId().equals(id))
-                .findFirst()
-                .orElse(null);
-    }
-
     public BookDTO addBook(BookDTO book) {
-        books.add(new Book(book.getTitle(), book.getAuthor()));
+        bookRepository.save(BookMapper.toBook(book));
         return book;
     }
 
     public boolean deleteBookById(Long id) {
-        return books.removeIf(book -> book.getId().equals(id));
+        if (bookRepository.existsById(id)) {
+            bookRepository.deleteById(id);
+            return true;
+        }
+        return bookRepository.existsById(id);
     }
 
     public BookDTO updateBook(BookDTO book, Long id) {
-        if (id == null) {
-            return null;
-        }
-
-        Book oldBook = getBookById(id);
-        if (oldBook == null) {
-            return null;
-        }
-
-        oldBook.setTitle(book.getTitle());
-        oldBook.setAuthor(book.getAuthor());
-        return BookMapper.toBookDTO(oldBook);
+        return bookRepository.findById(id)
+                .map(existingBook -> {
+                    existingBook.setTitle(book.getTitle());
+                    existingBook.setAuthor(book.getAuthor());
+                    Book updatedBook = bookRepository.save(existingBook);
+                    return BookMapper.toBookDTO(updatedBook);
+                })
+                .orElse(null);
     }
 
     public BookPatchDTO patchBook(BookPatchDTO book, Long id) {
-        if (id == null) {
-            return null;
-        }
-        Book oldBook = getBookById(id);
-        if (oldBook == null) {
-            return null;
-        }
-        oldBook.setTitle(book.getTitle() != null ? book.getTitle() : oldBook.getTitle());
-        oldBook.setAuthor(book.getAuthor() != null ? book.getAuthor() : oldBook.getAuthor());
-
-        return BookPatchMapper.toBookPatchDTO(oldBook);
+        return bookRepository.findById(id)
+                .map(existingBook -> {
+                    if (book.getTitle() != null) {
+                        existingBook.setTitle(book.getTitle());
+                    }
+                    if (book.getAuthor() != null) {
+                        existingBook.setAuthor(book.getAuthor());
+                    }
+                    Book updatedBook = bookRepository.save(existingBook);
+                    return BookPatchMapper.toBookPatchDTO(updatedBook);
+                })
+                .orElse(null);
     }
 }
