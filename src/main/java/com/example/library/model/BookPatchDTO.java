9463e4f29d6c6d95f87fef7072e3e0b413fd355e
Ivan Yarigo <yarigo2018@yandex.ru>
2025-07-05 09:14:54 +0300

    feat: added BookPatchDTO

---
+++ src/main/java/com/example/library/model/BookPatchDTO.java
@@ -0,0 +1,24 @@
+package com.example.library.model;
+
+import jakarta.validation.constraints.NotBlank;
+
+public class BookPatchDTO {
+    private String title;
+    private String author;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
