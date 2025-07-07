c46d722ea8234e525b381a0c463d0bc0562ee7df
Ivan Yarigo <yarigo2018@yandex.ru>
2025-07-07 22:09:37 +0300

    feat: add from(Book book) method to BookDTO record

--- src/main/java/com/example/library/model/BookDTO.java
+++ src/main/java/com/example/library/model/BookDTO.java
@@ -6,5 +6,7 @@ import jakarta.validation.constraints.NotNull;
 public record BookDTO(@NotNull(message = "Id must not be empty!")Long id,
                       @NotBlank(message = "Title must not be empty!") String title,
                       @NotBlank(message = "Author must not be empty!") String author) {
-
+    public static BookDTO from(Book book) {
+        return new BookDTO(book.getId(), book.getTitle(), book.getAuthor());
+    }
 }
