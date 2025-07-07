407dc31bfc86bd16682ec73a50a163e2fcdff29f
Ivan Yarigo <yarigo2018@yandex.ru>
2025-07-07 22:24:00 +0300

    refactor: remove toBook() from BookPatchMapper because suddenly we don't need it

--- src/main/java/com/example/library/service/BookPatchMapper.java
+++ src/main/java/com/example/library/service/BookPatchMapper.java
@@ -4,13 +4,6 @@ import com.example.library.model.Book;
 import com.example.library.model.BookPatchDTO;
 
 public class BookPatchMapper {
-    public static Book toBook(BookPatchDTO bookPatchDTO) {
-        if (bookPatchDTO == null) {
-            return null;
-        }
-        return new Book(bookPatchDTO.title(), bookPatchDTO.author());
-    }
-
     public static BookPatchDTO toBookPatchDTO(Book book) {
         if (book == null) {
             return null;
