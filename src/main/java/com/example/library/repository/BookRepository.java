9b924c3a4d165a05b38c6ff405fb1465914c4ba1
Ivan Yarigo <yarigo2018@yandex.ru>
2025-07-08 18:16:39 +0300

    feat: add findByAuthorAndTitle to BookRepository

--- src/main/java/com/example/library/repository/BookRepository.java
+++ src/main/java/com/example/library/repository/BookRepository.java
@@ -3,5 +3,8 @@ package com.example.library.repository;
 import com.example.library.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface BookRepository extends JpaRepository<Book, Long> {
+    Optional<Book> findByAuthorAndTitle(String author, String title);
 }
