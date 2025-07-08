8b8181b4c08256f2b85d9c0abe2a939aebb64591
Ivan Yarigo <yarigo2018@yandex.ru>
2025-07-08 16:28:51 +0300

    feat: add LoggingAspect with logBefore, logAfterReturning, logAfterThrowing methods

---
+++ src/main/java/com/example/library/aop/LoggingAspect.java
@@ -0,0 +1,32 @@
+package com.example.library.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    @Before("execution(* com.example.library.service.*.*(..))")
+    public void logBefore(JoinPoint joinPoint) {
+        Logger logger = LoggerFactory.getLogger(joinPoint.getTarget().getClass());
+        logger.info("Entering method: {} with args: {}", joinPoint.getSignature().getName(),  joinPoint.getArgs());
+    }
+
+    @AfterReturning(pointcut = "execution(* com.example.library.service.*.*(..))", returning = "result")
+    public void logAfterReturning(JoinPoint joinPoint, Object result) {
+        Logger logger = LoggerFactory.getLogger(joinPoint.getTarget().getClass());
+        logger.info("Method: {} with args: {} executed with result: {}", joinPoint.getSignature().getName(), joinPoint.getArgs(), result);
+    }
+
+    @AfterThrowing(pointcut = "execution(* com.example.library.service.*.*(..))", throwing = "ex")
+    public void logAfterThrowing(JoinPoint joinPoint, Exception ex) {
+        Logger logger = LoggerFactory.getLogger(joinPoint.getTarget().getClass());
+        logger.error("Method: {} with args: {} executed with exception: {}", joinPoint.getSignature().getName(), joinPoint.getArgs(), ex.getMessage());
+    }
+}
