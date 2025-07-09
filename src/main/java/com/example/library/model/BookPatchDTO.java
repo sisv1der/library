89128e528a5daaa7acf607841df040f5ac1174d9
Ivan Yarigo <yarigo2018@yandex.ru>
2025-07-09 13:13:08 +0300

    refactor: remove BookPostDTO.from, BookPatchDTO.from

--- src/main/java/com/example/library/model/BookPatchDTO.java
+++ src/main/java/com/example/library/model/BookPatchDTO.java
@@ -6,8 +6,4 @@ public record BookPatchDTO(
         @Schema(description = "The title of the book", example = "Sherlock Holmes")
         String title,
         @Schema(description = "The author of the book", example = "Arthur Conan Doyle")
-        String author) implements BookInput {
-    public static BookPatchDTO from(Book book) {
-        return new BookPatchDTO(book.getTitle(), book.getAuthor());
-    }
-}
+        String author) implements BookInput {}
--- src/main/java/com/example/library/model/BookPostDTO.java
+++ src/main/java/com/example/library/model/BookPostDTO.java
@@ -6,8 +6,4 @@ import jakarta.validation.constraints.NotBlank;
 public record BookPostDTO(@Schema(description = "The title of the book", example = "Sherlock Holmes")
                          @NotBlank(message = "Title must not be empty!") String title,
                           @Schema(description = "The author of the book", example = "Arthur Conan Doyle")
-                         @NotBlank(message = "Author must not be empty!") String author) implements BookInput {
-    public static BookPostDTO from(Book book) {
-        return new BookPostDTO(book.getTitle(), book.getAuthor());
-    }
-}
+                         @NotBlank(message = "Author must not be empty!") String author) implements BookInput {}
